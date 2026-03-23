dda48a69d4053918f411f21c0e4127efb49ba25a
teslacodes7 <satvik.manchanda@univest.in>
2026-03-24 02:44:34 +0530

    init

---
+++ src/main/java/com/example/sonicLens/SonicLensApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonicLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonicLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonicLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonicLens/SonicLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonicLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonicLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
